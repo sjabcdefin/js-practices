9de5f6497c072b9500b942723142075db1ab4c0d
tomoka <tomoka.saiwaki@gmail.com>
2025-03-08 22:33:26 +0900

    Remove unnecessary line breaks and indentation from createTableQuery
    createTableQueryから不要な改行とインデントを削除し、一行に統一

--- 04.class/memo_database.js
+++ 04.class/memo_database.js
@@ -1,12 +1,7 @@
 import sqlite3 from "sqlite3";
 import { runQuery, allQuery, getQuery, closeDatabase } from "./sqlite_utils.js";
 
-const createTableQuery = `
-CREATE TABLE IF NOT EXISTS memos (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  content TEXT NOT NULL
-)
-`;
+const createTableQuery = `CREATE TABLE IF NOT EXISTS memos (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT NOT NULL)`;
 const insertMemoQuery = `INSERT INTO memos (content) VALUES (?)`;
 const selectAllMemoQuery = `SELECT * FROM memos`;
 const selectMemoQuery = `SELECT * FROM memos WHERE id = (?)`;
