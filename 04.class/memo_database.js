e1c1d2f982330b0eb0531f6c3699da63a6ac09d9
tomoka <tomoka.saiwaki@gmail.com>
2025-03-08 22:35:34 +0900

    Fix the SQL query to use a regular string instead of a template string
    文字列中に変数を埋め込んでいないため

--- 04.class/memo_database.js
+++ 04.class/memo_database.js
@@ -1,11 +1,12 @@
 import sqlite3 from "sqlite3";
 import { runQuery, allQuery, getQuery, closeDatabase } from "./sqlite_utils.js";
 
-const createTableQuery = `CREATE TABLE IF NOT EXISTS memos (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT NOT NULL)`;
-const insertMemoQuery = `INSERT INTO memos (content) VALUES (?)`;
-const selectAllMemoQuery = `SELECT * FROM memos`;
-const selectMemoQuery = `SELECT * FROM memos WHERE id = (?)`;
-const deleteMemoQuery = `DELETE FROM memos WHERE id = (?)`;
+const createTableQuery =
+  "CREATE TABLE IF NOT EXISTS memos (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT NOT NULL)";
+const insertMemoQuery = "INSERT INTO memos (content) VALUES (?)";
+const selectAllMemoQuery = "SELECT * FROM memos";
+const selectMemoQuery = "SELECT * FROM memos WHERE id = (?)";
+const deleteMemoQuery = "DELETE FROM memos WHERE id = (?)";
 
 class MemoDatabase {
   #db;
