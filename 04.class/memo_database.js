1e6afdf973c84ca0bf014117ddd9617de69d1ffc
tomoka <tomoka.saiwaki@gmail.com>
2025-03-22 15:57:32 +0900

    Fix to define Query variables  in MemoDatabase class
    クエリは、一般的にクラス内に定義されるべきであるため

--- 04.class/memo_database.js
+++ 04.class/memo_database.js
@@ -1,13 +1,13 @@
 import sqlite3 from "sqlite3";
 import { runQuery, allQuery, closeDatabase } from "./sqlite_utils.js";
 
-const createTableQuery =
-  "CREATE TABLE IF NOT EXISTS memos (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT NOT NULL)";
-const insertMemoQuery = "INSERT INTO memos (content) VALUES (?)";
-const selectAllMemoQuery = "SELECT * FROM memos ORDER BY id";
-const deleteMemoQuery = "DELETE FROM memos WHERE id = ?";
-
 class MemoDatabase {
+  static createTableQuery =
+    "CREATE TABLE IF NOT EXISTS memos (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT NOT NULL)";
+  static insertMemoQuery = "INSERT INTO memos (content) VALUES (?)";
+  static selectAllMemoQuery = "SELECT * FROM memos ORDER BY id";
+  static deleteMemoQuery = "DELETE FROM memos WHERE id = ?";
+
   #db;
 
   constructor(dbPath) {
@@ -16,19 +16,19 @@ class MemoDatabase {
   }
 
   async createTable() {
-    await runQuery(this.#db, createTableQuery);
+    await runQuery(this.#db, MemoDatabase.createTableQuery);
   }
 
   async insert(content) {
-    return await runQuery(this.#db, insertMemoQuery, [content]);
+    return await runQuery(this.#db, MemoDatabase.insertMemoQuery, [content]);
   }
 
   async selectAll() {
-    return await allQuery(this.#db, selectAllMemoQuery);
+    return await allQuery(this.#db, MemoDatabase.selectAllMemoQuery);
   }
 
   async delete(id) {
-    await runQuery(this.#db, deleteMemoQuery, [id]);
+    await runQuery(this.#db, MemoDatabase.deleteMemoQuery, [id]);
   }
 
   async closeDatabase() {
