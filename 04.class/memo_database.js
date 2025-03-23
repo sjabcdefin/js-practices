a19282778152bce3c7bbe558ee2ce2b1acd9d5f9
tomoka <tomoka.saiwaki@gmail.com>
2025-03-23 19:29:18 +0900

    Fix the query variables to private variables
    クラス外からの参照がないため

--- 04.class/memo_database.js
+++ 04.class/memo_database.js
@@ -2,11 +2,11 @@ import sqlite3 from "sqlite3";
 import { runQuery, allQuery, closeDatabase } from "./sqlite_utils.js";
 
 class MemoDatabase {
-  static createTableQuery =
+  static #createTableQuery =
     "CREATE TABLE IF NOT EXISTS memos (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT NOT NULL)";
-  static insertMemoQuery = "INSERT INTO memos (content) VALUES (?)";
-  static selectAllMemoQuery = "SELECT * FROM memos ORDER BY id";
-  static deleteMemoQuery = "DELETE FROM memos WHERE id = ?";
+  static #insertMemoQuery = "INSERT INTO memos (content) VALUES (?)";
+  static #selectAllMemoQuery = "SELECT * FROM memos ORDER BY id";
+  static #deleteMemoQuery = "DELETE FROM memos WHERE id = ?";
 
   #db;
 
@@ -16,19 +16,19 @@ class MemoDatabase {
   }
 
   async createTable() {
-    await runQuery(this.#db, MemoDatabase.createTableQuery);
+    await runQuery(this.#db, MemoDatabase.#createTableQuery);
   }
 
   async insert(content) {
-    return await runQuery(this.#db, MemoDatabase.insertMemoQuery, [content]);
+    return await runQuery(this.#db, MemoDatabase.#insertMemoQuery, [content]);
   }
 
   async selectAll() {
-    return await allQuery(this.#db, MemoDatabase.selectAllMemoQuery);
+    return await allQuery(this.#db, MemoDatabase.#selectAllMemoQuery);
   }
 
   async delete(id) {
-    await runQuery(this.#db, MemoDatabase.deleteMemoQuery, [id]);
+    await runQuery(this.#db, MemoDatabase.#deleteMemoQuery, [id]);
   }
 
   async closeDatabase() {
